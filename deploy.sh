311dd2aca8affc4c3165b270bb481342f7301379
Paula Grangeiro <contato@paulagrangeiro.com.br>
2017-06-01 04:05:21 -0300

    Update travis setup

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-git clone https://${GITHUB_TOKEN}@github.com:pgrangeiro/pgrangeiro.github.io.git ../tmp
-cd ../tmp
+git clone https://${GITHUB_TOKEN}@github.com:pgrangeiro/pgrangeiro.github.io.git tmp
+cd tmp
 git checkout master
 rm -rf *
 
-cp -R ../pgrangeiro.github.io/index.html .
-cp -R ../pgrangeiro.github.io/CNAME .
-cp -R ../pgrangeiro.github.io/LICENSE.md .
-cp -R ../pgrangeiro.github.io/assets .
+cp ~/pgrangeiro/pgrangeiro.github.io/index.html .
+cp ~/pgrangeiro/pgrangeiro.github.io/CNAME .
+cp ~/pgrangeiro/pgrangeiro.github.io/LICENSE.md .
+cp -R ~/pgrangeiro/pgrangeiro.github.io/assets .
 
 git status
 git add -A .
