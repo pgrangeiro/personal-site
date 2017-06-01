3a815bf764d760449c8b7465dc6306fa4b3f7e12
Paula Grangeiro <contato@paulagrangeiro.com.br>
2017-06-01 03:43:47 -0300

    Update travis setup

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,17 @@ git clone https://${GITHUB_TOKEN}@github.com:pgrangeiro/pgrangeiro.github.io.git
 cd ../tmp
 git checkout master
 rm -rf *
+echo "Cloned master"
 
 cp -R ../pgrangeiro.github.io/_site/index.html .
 cp -R ../pgrangeiro.github.io/_site/CNAME .
 cp -R ../pgrangeiro.github.io/_site/LICENSE.md .
 cp -R ../pgrangeiro.github.io/_site/assets .
+echo "Copy files"
 
 git status
 git add -A .
 git status
 git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
 git push -f origin `master`
+echo "Finished push"
